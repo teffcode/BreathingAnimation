5f1caea287e625d5d63685f0acacac2bc495d1b5
teffcode <tefy.1@live.com>
2017-12-13 21:40:18 -0500

    First Commit

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Merry Christmas</h1>
+        <div className="breathing"/>
+      </div>
+    );
+  }
+}
+
+export default App;
